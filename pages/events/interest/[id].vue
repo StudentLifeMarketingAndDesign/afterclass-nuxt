a214788a983498a57bd9e821c3a439a30f1fdd19
Dustin Quam <quamsta@gmail.com>
2022-11-04 11:24:44 -0500

    removed link from late night programs page

--- pages/events/interest/[id].vue
+++ pages/events/interest/[id].vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-
     <Head>
       <Title>{{ interest.name }}</Title>
       <MetaImageDefault />
@@ -13,23 +12,23 @@
         <div class="col-lg-7 m-auto">
           <div class="late-night-feature my-4" v-if="interest.id == 7491">
             <p>
-              <img class="late-night-feature__flag" src="~/assets/images/latenightbanner.png"
-                alt="Late Night Programs Flag" role="presentation" />The purpose of Late Night and Weekend Programs is
-              to promote the
+              <img
+                class="late-night-feature__flag"
+                src="~/assets/images/latenightbanner.png"
+                alt="Late Night Programs Flag"
+                role="presentation"
+              />The purpose of Late Night and Weekend Programs is to promote the
               health, well-being and safety of students by providing
               opportunities to engage with others and build community in an
               environment without alcohol.
             </p>
-            <ul>
-              <li>
-                <a href="https://leadandengage.uiowa.edu/our-programs/late-night-weekend-programs" target="_blank">Learn
-                  more on the Late Night and Weekend Programs website.</a>
-              </li>
-              <li>
-                <NuxtLink href="/events/interest/7491/">Find more events tagged as Late Night and Weekend
-                  Programs.</NuxtLink>
-              </li>
-            </ul>
+            <p>
+              <a
+                href="https://leadandengage.uiowa.edu/our-programs/late-night-weekend-programs"
+                target="_blank"
+                >Learn more on the Late Night and Weekend Programs website.</a
+              >
+            </p>
 
             <p class="late-night-feature__smalltext"></p>
           </div>
@@ -67,7 +66,6 @@ if (interestsFeed.value["event_general_interest"].length > 0) {
 </script>
   
 <style scoped>
-
 </style>
 
 
